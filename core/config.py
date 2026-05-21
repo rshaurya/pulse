7a3e2152649cf4b85b7d31e697ed08fc83a07239
rshaurya24 <shaurya.r.pethe@gmail.com>
2026-05-21 23:07:43 +0530

    changed vector embed size to 384 and import api keys from dot env. print statements for debugging

--- core/config.py
+++ core/config.py
@@ -1,15 +1,40 @@
-from pydantic import BaseModel
+import os
 
-class Settings(BaseModel):
-    PROJECT_NAME: str = "PULSE Knowledge Engine"
-    # Qdrant is running locally via Docker
-    QDRANT_HOST: str = "localhost"
-    QDRANT_PORT: int = 6333
-    COLLECTION_NAME: str = "knowledge_base"
-    VECTOR_SIZE: int = 3072
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_FILE_PATH = os.path.join(BASE_DIR, ".env")
+
+print("="*50)
+print(f"[DEBUG] Looking for .env file at: {ENV_FILE_PATH}")
+print(f"[DEBUG] Does file exist? {os.path.exists(ENV_FILE_PATH)}")
+print("="*50)
+
+load_dotenv(ENV_FILE_PATH)
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "PULSE"
     
-    # Ollama is running locally via Docker
-    OLLAMA_BASE_URL: str = "http://localhost:11434"
+    # Qdrant cloud settings
+    QDRANT_URL: str = "http://localhost:6333"
+    QDRANT_API_KEY: str | None = None
+    COLLECTION_NAME: str = "pulse_intel"
+    VECTOR_SIZE: int = 384
+    
+    # Cloud LLM Settings
+    LLM_BASE_URL: str = "http://localhost:11434/api/chat"
+    LLM_API_KEY: str | None = None
     LLM_MODEL: str = "phi3:mini" 
+    
+    # Web Search Config (Pipeline B)
+    TAVILY_API_KEY: str | None = None 
+    
 
 settings = Settings()
+
+print("="*50)
+print(f"[SYSTEM BOOT] Database URL: {settings.QDRANT_URL}")
+print(f"[SYSTEM BOOT] AI Brain URL: {settings.LLM_BASE_URL}")
+print("="*50)
