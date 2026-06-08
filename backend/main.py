c06abb555c22d05de28cfaa16821c9d5ef41f897
rshaurya24 <shaurya.r.pethe@gmail.com>
2026-06-08 15:01:11 +0530

    added deployed frontend url to allow origins

--- backend/main.py
+++ backend/main.py
@@ -63,7 +63,7 @@ app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000", "https://www.pulse-ai.me/"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"] # improve for production!
